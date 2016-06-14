3f4c3668457c1da332d01c4bb96333e507ffe972
Dave McFarland <david.mcfarland@simp.dev>
2016-06-13 21:44:18 -0400

    Corrected gpio pins

--- rotvol.py
+++ rotvol.py
@@ -8,12 +8,12 @@ from rotary_class import RotaryEncoder
 from subprocess import call
 
 # Define GPIO inputs
-RPIN_A = 14  # Pin 8
-RPIN_B = 15  # Pin 10
-RBUTTON = 4  # Pin 7
-LPIN_A = 14  # Pin 8
-LPIN_B = 15  # Pin 10
-LBUTTON = 4  # Pin 7
+RPIN_A = 24  # Pin 8
+RPIN_B = 25  # Pin 10
+RBUTTON = 23  # Pin 7
+LPIN_A = 5  # Pin 8
+LPIN_B = 6  # Pin 10
+LBUTTON = 13  # Pin 7
 
 # This is the event callback routine to handle events
 def vol_event(event):
@@ -24,20 +24,18 @@ def vol_event(event):
     elif event == RotaryEncoder.BUTTONDOWN:
         pass
     elif event == RotaryEncoder.BUTTONUP:
-        print "Button up"
         call(["mpc", "toggle"], shell=False)
     return
 
 def switch_event(event):
     if event == RotaryEncoder.CLOCKWISE:
-        call(["mpc", "vol", "+2"], shell=False)
+        call(["mpc", "next"], shell=False)
     elif event == RotaryEncoder.ANTICLOCKWISE:
-        call(["mpc", "vol", "-2"], shell=False)
+        call(["mpc", "prev"], shell=False)
     elif event == RotaryEncoder.BUTTONDOWN:
         pass
     elif event == RotaryEncoder.BUTTONUP:
-        print "Button up"
-        call(["mpc", "toggle"], shell=False)
+        call(["mpc", "random"], shell=False)
     return
 
 # Define the right switch
